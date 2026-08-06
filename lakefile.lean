8d68fd26e2f8b6c3af8ac1620fe99b94010dadaf
Jonathan Cubides <prieto.jona@gmail.com>
2026-08-06 02:01:02 -0500

    release: v0.2.3

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 package «grip-diagnostics» where
-  version := v!"0.2.2"
+  version := v!"0.2.3"
   leanOptions := #[⟨`autoImplicit, false⟩, ⟨`relaxedAutoImplicit, false⟩]
 
 require grip from git
